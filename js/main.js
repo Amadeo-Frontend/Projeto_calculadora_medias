5ac56c0e5327dfbff4a2e02a48121f3466a6a6e8
Amadeo Bon <amadeosph@outlook.com>
2023-01-16 17:28:38 -0300

    fix:limitação das casa decimais

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ function atualizaTabela() {
 function atualizaMediaFinal() {
   const mediaFinal = calculaMediaFinal();
 
-  document.getElementById("media-final-valor").innerHTML = mediaFinal;
+  document.getElementById("media-final-valor").innerHTML = mediaFinal.toFixed(2);
   document.getElementById("media-final-resultado").innerHTML =
     mediaFinal >= notaMinima ? spanAprovado : spanReprovado;
 }
